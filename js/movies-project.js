3d1e9164b76667e56ddf61bd3b6169712b21d644
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-10-06 12:53:25 -0500

    Added Favorite movies search in the navbar

--- js/movies-project.js
+++ js/movies-project.js
@@ -2,55 +2,14 @@
 // alert("Welcome to the movies project");
 
 $(function(){
-    /*API --- Application Programming Interface
-    * a set of rules defining how applications and/ or devices can connect ot each other and communicate with one another
-    *
-    * REST means Representational State Transfer
-    *A pattern of design principles for APIs
-    *REST APIs communicate via HTTP requests
-    *
-    *In a REST API communication via HTTP reqeuests is used to perfomr the standard set of actions called CRUD
-    * Create, Read, Update, Delete
-    * HTTP only enables two kinds of request GET and POST
-    *
-    * REST API creates different request types
-    *
-    * JSON Javascript Object Notation - is not mandatory but is popular becuase
-    * it is both human and machine-readable
-    * */
 
-//https://glitch.com/stingy-prickle-sternum
 //URL constants:
-    const booksURL = "https://stingy-prickle-sternum.glitch.me/books"
+
 
 
     const moviesURL = "https://stingy-prickle-sternum.glitch.me/movies"
-    //
-
-    // $("#userNameInput").keyup(function(e){
-//     console.log(e.key)
-//     if (e.key === "Enter"){
-//         // userName = $(this).val();
-//         // usersLastCommitDate = gitUserLastCommitAsync(userName);
-//         gitUserLastCommitAsync($(this).val()).then(response => $("#output").text(`${response}`))/
-//     }
-//
-// })
-
-    // $("#userFavMovie").keyup(function(e){
-    //     console.log(e.key)
-    //     if(e.key === "Enter"){
-    //         theMoviesDataBaseURL($(this).val()).then(response =>$(#movies.a))
-    //     }
-    // })
 
 
-
-
-
-
-
-//The R in CRUD is Read
 
     /*=====================FUNCTION THAT PRODUCES MOVIE CARDS=============*/
 
@@ -158,7 +117,7 @@ $(function(){
 //
     function postMovie() {
         $("#userFavMovie").keyup(function (e) {
-            let usersMovie
+            let usersMovie;
             if (e.key === "Enter") {
                 usersMovie = $(this).val();
                 theMoviesDataBaseURL(usersMovie);
@@ -168,7 +127,7 @@ $(function(){
 
     postMovie()
 
-    let poster
+    let poster;
     function theMoviesDataBaseURL(userSearch){
         fetch(`https://api.themoviedb.org/3/search/movie${J_TBD_TOKEN}&query=${userSearch}&include_adult=false`)
             .then(response => response.json())
@@ -196,88 +155,6 @@ $(function(){
                 fetch(moviesURL, putOptions).then(getMovies)
             })
     }
-
-
-
-    // async function gitUserLastCommitAsync(username){
-    //     let response = await fetch(`https://api.github.com/users/${username}/events/public`,{headers: {"Authorization": GITHUB_PROMISES_TOKEN}});
-    //     response = await response.json();
-    //     let events = response;
-    //     const dateOfLastCommit = new Date(events[0].created_at);
-    //     console.log(dateOfLastCommit)
-    //     return dateOfLastCommit;
-    // }
-    //
-    // https://api.themoviedb.org/3/movie/550?api_key=
-
-    // $("#userNameInput").keyup(function(e){
-    //     // console.log(e.key)
-    //     if (e.key === "Enter"){
-    //         // userName = $(this).val();
-    //         // usersLastCommitDate = gitUserLastCommitAsync(userName);
-    //         gitUserLastCommitAsync($(this).val()).then(response => $("#output").text(`${response}`))
-    //     }
-    //
-    // })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   // let usersMovie = {
-   //      title: `${$("#userFavMovie").val()}`,
-   //
-   //  }
-   //
-   //  const putOptions = {
-   //      method: "PUT",
-   //      headers: {
-   //          "Content-Type": "application/json"
-   //      },
-   //      body: JSON.stringify(modification)
-   //  }
-
-//This will put the updated modifications to the 1st book
-// fetch(booksURL +"/1", putOptions).then(getBooks);
-
-//Loading Message: setTimeout
-
-//Put the movies in cards: Use Bootstrap
-//Recommend using Fetch
-//Use Promises
-//Form will be able to post a movie (similiar to how we posted a book)
-//Feed into an object
-//Event Driven Ajax Requests
-//Then move on to DELETE
-//we need the ID to delete something
-//Where do you put the ID so when the user clicks on the delete button, it sends the iD to the request
-//When printing on the screen set the ID of the movie to the button
-//button data-id = 1
-//After deleting something, edit will be similiar
-//data atrribute on the div
-    /*data property[#ofmovieID]
-
-    * put the fetch inside of a function
-    *
-    *
-    *
-    *
-    *
-    *
-    *
-    *
-    * */
-
-
 
 
 })//End of document.ready
